7955b6b27afb07464e4680fe97959bd45cb6982d
anouar elkhalfi <anouarelkhalfi@gmail.com>
2024-05-25 12:00:00 +0200

    admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :restaurants
+      resources :reviews
+
+      root to: "restaurants#index"
+    end
   root to: "restaurants#index"
   resources :restaurants, only: [:index, :show, :new, :create] do
     resources :reviews, only: [:create]
